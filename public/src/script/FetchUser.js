cb266c45005895f6cf84dc6d1a5ca70655d3de2b
Gorthdzx <krislaksana123@gmail.com>
2024-02-29 06:09:58 +0700

    feat : fetch amount of user repos

--- public/src/script/FetchUser.js
+++ public/src/script/FetchUser.js
@@ -1,6 +1,10 @@
 const endpoint = "https://api.github.com/users/KrishnanditoLksn"
+const url = "https://api.github.com/users"
 const h1 = document.querySelector('.github_fetch')
 const images = document.querySelector('#image')
+const repoId = document.getElementById('repos')
+// const usernameTextfield = document.getElementById('username')
+// const submit = document.getElementById('submit')
 
 async function getGithubUsername() {
     const response = await fetch(endpoint)
@@ -20,10 +24,19 @@ async function getAvatarImages() {
     const response = await fetch(imageUrl)
     const imageBlob = response.blob()
     images.src = URL.createObjectURL(await imageBlob)
-    images.width = 100
+    images.width = 250
 }
 
 getAvatarImages().catch(err => {
     console.log(err)
 })
 
+async function getRepos() {
+    const res = await fetch(endpoint)
+    const datasRepos = await res.json()
+    repoId.textContent = datasRepos.public_repos
+}
+
+getRepos().catch(err => {
+    console.log(err)
+})
