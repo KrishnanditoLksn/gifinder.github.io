ef4c29570563f6099cb3591e4d5b1e2953d9f556
Gorthdzx <krislaksana123@gmail.com>
2024-02-29 14:20:59 +0700

    feat : added search feature  by username with github user API

--- public/src/script/FetchUser.js
+++ public/src/script/FetchUser.js
@@ -1,42 +1,25 @@
 const endpoint = "https://api.github.com/users/KrishnanditoLksn"
-const url = "https://api.github.com/users"
+const url = "https://api.github.com/users/"
 const h1 = document.querySelector('.github_fetch')
 const images = document.querySelector('#image')
 const repoId = document.getElementById('repos')
-// const usernameTextfield = document.getElementById('username')
-// const submit = document.getElementById('submit')
+const usernameTextfield = document.getElementById('username')
+const submit = document.getElementById('submit')
 
-async function getGithubUsername() {
-    const response = await fetch(endpoint)
+async function getGithubUsername(username) {
+    const response = await fetch(url + username)
     if (response.ok) {
         let data = await response.json()
         h1.textContent = data.login
+        repoId.textContent = data.public_repos
+        images.src = data.avatar_url
     }
+
 }
 
-getGithubUsername().catch(err => {
-    console.log(err)
-})
-
-//get images from api
-async function getAvatarImages() {
-    const imageUrl = "https://avatars.githubusercontent.com/u/96246664?v=4"
-    const response = await fetch(imageUrl)
-    const imageBlob = response.blob()
-    images.src = URL.createObjectURL(await imageBlob)
-    images.width = 250
-}
-
-getAvatarImages().catch(err => {
-    console.log(err)
-})
-
-async function getRepos() {
-    const res = await fetch(endpoint)
-    const datasRepos = await res.json()
-    repoId.textContent = datasRepos.public_repos
-}
-
-getRepos().catch(err => {
-    console.log(err)
+submit.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    getGithubUsername(usernameTextfield.value).catch(err => {
+        console.log(err)
+    })
 })
